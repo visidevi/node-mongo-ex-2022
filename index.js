7b52921df97664a6660afa67432a7a6b6129b89a
visidevi <visidevi1@gmail.com>
2022-03-09 12:49:06 -0300

    HTML Templating: Filling the Templates

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const url = require("url");
 /////////////////////////////////
 
 // FILES
-
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) console.error(err, "ERROR");
   console.log(data);
@@ -14,11 +13,22 @@ console.log("Hello World!");
 // const datasync = fs.readFileSync("input.txt", "utf8");
 // console.log(datasync);
 // Server response
+
+const apiData = fs.readFileSync("./data/data.json", "utf8");
+const apiDataJson = JSON.parse(apiData);
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
   const pathName = req.url;
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(apiDataJson);
+    // fs.readFile("./data/data.json", "utf8", (err, data) => {
+    //   const obj = JSON.parse(data);
+    //   res.end(JSON.stringify(obj));
+    // })
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
   } else {
