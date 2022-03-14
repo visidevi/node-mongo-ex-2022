50990650eb7282c662f4fa26f6ff583e6348b3cc
visidevi <visidevi1@gmail.com>
2022-03-14 10:39:24 -0300

    Making the API Better: Limiting Fields

--- express/controllers/tours.js
+++ express/controllers/tours.js
@@ -3,12 +3,45 @@ const Tour = require('../models/tours');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+    // Build query
+    // 1> Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2> Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3> Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+    //  4> Field limiting
+    if (req.query.fields) {
+      console.log(req.query.fields);
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // Execute query
+    const tours = await query;
+    // Send Response
+
+    // const tours = await Tour.find();
     res.status(200).json({
       status: 'success',
       data: {
-        tours,
         results: tours.length,
+        tours,
       },
     });
   } catch (err) {
