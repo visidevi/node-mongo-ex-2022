31e26267f8fee06b989fea3c0c2e49a800b654ff
visidevi <visidevi1@gmail.com>
2022-03-14 11:49:44 -0300

    Aliasing

--- express/controllers/tours.js
+++ express/controllers/tours.js
@@ -1,6 +1,13 @@
 // Models
 const Tour = require('../models/tours');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
@@ -30,6 +37,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+    // 5> Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    console.log(skip);
+    query = query.skip(skip).limit(limit);
+    console.log('The query', query);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     // Execute query
