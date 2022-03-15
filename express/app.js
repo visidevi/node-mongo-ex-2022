c7aeddf6d111991b84265cb09555ca09be2d84db
visidevi <visidevi1@gmail.com>
2022-03-14 22:10:35 -0300

    Better Errors and Refactoring

--- express/app.js
+++ express/app.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-dynamic-require */
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/error');
 
 const app = express();
 
@@ -13,10 +15,7 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+// Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -33,4 +32,18 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/tours', toursRouter);
 
+// Router error Handler
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 module.exports = app;
