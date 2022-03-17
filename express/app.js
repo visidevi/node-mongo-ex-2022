4b919e0280772f8a9db6baf30ff2e62550fdf317
visidevi <visidevi1@gmail.com>
2022-03-17 18:32:47 -0300

    Data Sanitization

--- express/app.js
+++ express/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitizer = require('express-mongo-sanitize');
+const XSS = require('xss-clean');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/error');
 
@@ -26,6 +29,12 @@ app.use('/api', limiter);
 
 // Body parser reading data from body into re.body
 app.use(express.json({ limit: '10kb' }));
+
+//  Data Sanitization against NoSQL query injection
+app.use(mongoSanitizer());
+
+// Data Sanitization against XSS
+app.use(XSS());
 // Serving statis files
 app.use(express.static(`${__dirname}/public`));
 
