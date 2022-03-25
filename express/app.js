4c398c0dfbc025bfa4ece2ceff716e0c784ab849
devi <visidevi1@gmail.com>
2022-03-25 14:06:17 -0300

    pug

--- express/app.js
+++ express/app.js
@@ -1,84 +1,154 @@
-/* eslint-disable import/no-dynamic-require */
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitizer = require('express-mongo-sanitize');
-const XSS = require('xss-clean');
-const hpp = require('hpp');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/error');
+require("@babel/polyfill");
+var $cQivH$axios = require("axios");
 
-// Routing
-const usersRouter = require(`${__dirname}/routes/users`);
-const toursRouter = require(`${__dirname}/routes/tours`);
-const reviewsRouter = require(`${__dirname}/routes/reviews.router`);
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
 
-const app = express();
+const $b521082dd449d16e$export$4c5dd147b21b9176 = (locations)=>{
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2aTEwOCIsImEiOiJjbDE2ZXM1aWg0NGpuM2NycHQxaXlhNGM1In0.mYrTwuzfGjaVc-9UJ9Xr9Q';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/devi108/cl16gcze8000g14nanovors5c',
+        //style: 'mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy',
+        scrollZoom: false
+    });
+    const bounds = new mapboxgl.LngLatBounds();
+    locations.forEach((loc)=>{
+        // Create marker
+        const el = document.createElement('div');
+        el.className = 'marker';
+        // Add marker
+        new mapboxgl.Marker({
+            element: el,
+            anchor: 'bottom'
+        }).setLngLat(loc.coordinates).addTo(map);
+        // Add popup
+        new mapboxgl.Popup({
+            offset: 30
+        }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`).addTo(map);
+        // Extend map bounds to include current location
+        bounds.extend(loc.coordinates);
+    });
+    map.fitBounds(bounds, {
+        padding: {
+            top: 200,
+            bottom: 150,
+            left: 100,
+            right: 100
+        }
+    });
+};
 
-//1) Global Middleware
-// Security http headers
-app.use(helmet());
 
-// Development login Logger style morgan middleware
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-// Limit requests from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000, // 1 hour
-  message: 'Too many requests from this IP, please try again in an hour!',
+const $cf8ea27b34b2137b$export$516836c6a9dfc573 = ()=>{
+    const el = document.querySelector('.alert');
+    if (el) el.parentElement.removeChild(el);
+};
+const $cf8ea27b34b2137b$export$de026b00723010c1 = (type, msg)=>{
+    $cf8ea27b34b2137b$export$516836c6a9dfc573();
+    const markup = `<div class="alert alert--${type}">${msg}</div>`;
+    document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+    window.setTimeout($cf8ea27b34b2137b$export$516836c6a9dfc573, 5000);
+};
+
+
+const $433b644962c26f49$export$596d806903d1f59e = async (email, password)=>{
+    try {
+        const res = await ($parcel$interopDefault($cQivH$axios))({
+            method: 'POST',
+            url: 'http://127.0.0.1:3000/api/v1/users/login',
+            data: {
+                email: email,
+                password: password
+            }
+        });
+        if (res.data.status === 'success') {
+            $cf8ea27b34b2137b$export$de026b00723010c1('success', 'Logged in successfully!');
+            window.setTimeout(()=>{
+                location.assign('/');
+            }, 1500);
+        }
+    } catch (err) {
+        $cf8ea27b34b2137b$export$de026b00723010c1('error', err.response.data.message);
+    }
+};
+const $433b644962c26f49$export$a0973bcfe11b05c9 = async ()=>{
+    try {
+        const res = await ($parcel$interopDefault($cQivH$axios))({
+            method: 'GET',
+            url: 'http://127.0.0.1:3000/api/v1/users/logout'
+        });
+        res.data.status = 'success';
+        location.reload(true);
+    } catch (err) {
+        console.log(err.response);
+        $cf8ea27b34b2137b$export$de026b00723010c1('error', 'Error logging out! Try again.');
+    }
+};
+
+
+
+
+const $6842e7be16478138$export$f558026a994b6051 = async (data, type)=>{
+    try {
+        const url = type === 'password' ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword' : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        const res = await ($parcel$interopDefault($cQivH$axios))({
+            method: 'PATCH',
+            url: url,
+            data: data
+        });
+        if (res.data.status === 'success') $cf8ea27b34b2137b$export$de026b00723010c1('success', `${type.toUpperCase()} updated successfully!`);
+    } catch (err) {
+        $cf8ea27b34b2137b$export$de026b00723010c1('error', err.response.data.message);
+    }
+};
+
+
+// DOM ELEMENTS
+const $c74e663a61ed842a$var$mapBox = document.getElementById('map');
+const $c74e663a61ed842a$var$loginForm = document.querySelector('.form--login');
+const $c74e663a61ed842a$var$logOutBtn = document.querySelector('.nav__el--logout');
+const $c74e663a61ed842a$var$userDataForm = document.querySelector('.form-user-data');
+const $c74e663a61ed842a$var$userPasswordForm = document.querySelector('.form-user-password');
+// DELEGATION
+if ($c74e663a61ed842a$var$mapBox) {
+    const locations = JSON.parse($c74e663a61ed842a$var$mapBox.dataset.locations);
+    $b521082dd449d16e$export$4c5dd147b21b9176(locations);
+}
+if ($c74e663a61ed842a$var$loginForm) $c74e663a61ed842a$var$loginForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    $433b644962c26f49$export$596d806903d1f59e(email, password);
+});
+if ($c74e663a61ed842a$var$logOutBtn) $c74e663a61ed842a$var$logOutBtn.addEventListener('click', $433b644962c26f49$export$a0973bcfe11b05c9);
+if ($c74e663a61ed842a$var$userDataForm) $c74e663a61ed842a$var$userDataForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    $6842e7be16478138$export$f558026a994b6051({
+        name: name,
+        email: email
+    }, 'data');
+});
+if ($c74e663a61ed842a$var$userPasswordForm) $c74e663a61ed842a$var$userPasswordForm.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await $6842e7be16478138$export$f558026a994b6051({
+        passwordCurrent: passwordCurrent,
+        password: password,
+        passwordConfirm: passwordConfirm
+    }, 'password');
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
 });
 
-app.use('/api', limiter);
 
-// Body parser reading data from body into re.body
-app.use(express.json({ limit: '10kb' }));
-
-//  Data Sanitization against NoSQL query injection
-app.use(mongoSanitizer());
-
-// Data Sanitization against XSS
-app.use(XSS());
-// Prevent parameter pollution when using
-app.use(
-  hpp({
-    whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price',
-    ],
-  })
-);
-// Serving statis files
-app.use(express.static(`${__dirname}/public`));
-
-// Test Middleware
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  console.log(req.headers);
-  next();
-  console.log(req.requestTime);
-});
-
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/tours', toursRouter);
-app.use('/api/v1/reviews', reviewsRouter);
-
-// Route Handler
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getOneTour);
-// app.patch('/api/v1/tours/:id', patchTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-// Router error Handler
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
-
-app.use(globalErrorHandler);
-module.exports = app;
+//# sourceMappingURL=app.js.map
